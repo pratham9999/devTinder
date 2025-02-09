bd18a5563efdd8158b90aed1be6332a5f81b9ed4
Pratham kothari <en20it301079@medicaps.ac.in>
2025-02-09 22:33:12 +0530

    apis

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 import express from "express"
 import cors from "cors"
+import  {connectDB}  from "./config/database.js" 
+import dotenv from "dotenv"
+import userModel from "./models/user.js"
+
+
+dotenv.config()
 const app = express();
+
+
 
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/" , (req, res)=>{
-     res.send("Hello")
+
+app.post("/signup" , async (req, res)=>{
+
+    
+    const user = req.body
+    
+    
+    const users =await  userModel.create(user)
+
+    res.send("user added succesfully")
+    
+      
 })
 
-app.listen(3000 , ()=>{
-    console.log("Server is succeesfully listening on port 3000....");
-    
+//Feed api 
+app.get("/feed" , async  (req , res)=>{
+         const user = await userModel.find({})
+         res.json({
+             user
+         }) 
 })
+ 
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    firstName :{
+        type:String
+    } ,
+    lastName :{
+        type:String
+    } ,
+    emailId :{
+        type:String
+    } ,
+    password :{
+        type:String
+    } ,
+    age :{
+        type:String
+    } ,
+    gender :{
+        type:String
+    } ,
+
+})
+
+const userModel = mongoose.model("User" , userSchema)
+export default userModel
